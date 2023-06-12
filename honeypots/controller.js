250ab568c29f4de44ba433c1b26aa00f4e7f5061
savva <savva.lambin@gmail.com>
2023-06-12 12:09:28 +0100

    Add rate limiter, honeypot validation

--- honeypots/controller.js
+++ honeypots/controller.js
@@ -1,8 +1,17 @@
-const express = require('express');
-const router = express.Router();
-
 const signup = (req, res) => {
-  res.json('honeypot');
+  console.log(req.rateLimit.remaining);
+  if (req.rateLimit.remaining === 0) {
+    return res.status(429).send('Too many requests, please try again later');
+  }
+  // TESTING PURPOSE VARIABLE:
+  const honeyPotValue = true;
+  if (honeyPotValue) {
+    // Handle Spam:
+    res.status(403).json(`honeypot value is ${honeyPotValue}`);
+  } else {
+    console.log(req.body.name);
+    res.status(200).json(req.body.name);
+  }
 };
 
 module.exports = { signup };
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 5, // Limit each IP to 5 requests per windowMS
+  message: 'Too many requests from this IP, please try again later',
+});
 
 // To Use Body Data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Apply limiter to all requests:
+app.use(limiter);
+
+// Use Frontend
 app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/validation-messages', require('./routes/validationMessagesRoutes'));
